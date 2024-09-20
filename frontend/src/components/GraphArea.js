bb856b0cedd3790879cd90937789e4f3d69a061e
Jack <jack.stuart.braga@gmail.com>
2024-09-20 18:30:26 +1000

    Prepare for sliders and style the plot-type-heading

--- frontend/src/components/GraphArea.js
+++ frontend/src/components/GraphArea.js
@@ -1,37 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { simulateMonteCarlo } from '../APIs/pythonSimulation';
-import Histogram from './Histogram';
+import React, { useEffect, useState } from "react";
+import { simulateMonteCarlo } from "../APIs/pythonSimulation";
+import Histogram from "./Histogram";
+import GraphTypeSelector from "./GraphTypeSelector";
+import Slider from "./Slider";
 
 const GraphArea = ({ variables }) => {
-    const [results, setResults] = useState(null);
-    const [histogramData, setHistogramData] = useState(null);
-  
-    const handleSimulation = async () => {
-    	const data = await simulateMonteCarlo(variables, 2130, 50);
-        setResults(data);
-        setHistogramData(data);
-    };
-  
-    useEffect(() => {
-      handleSimulation();
-    }, []);
-    
-  
-    return (
-      <div className='graphArea'>
+  const [results, setResults] = useState(null);
+  const [histogramData, setHistogramData] = useState(null);
+
+  const handleSimulation = async () => {
+    const data = await simulateMonteCarlo(variables, 2130, 50);
+    setResults(data);
+    setHistogramData(data);
+  };
+
+  useEffect(() => {
+    handleSimulation();
+  }, []);
+
+  return (
+    <div className="graphArea">
+      {/* <h1>Monte Carlo Simulation Histogram</h1> */}
+      <GraphTypeSelector />
+      <div className="graphOptions">
+        <Slider />
+        {/* Number of Bins slider */}
+        {/* Number of Simulations slider */}
         <button onClick={handleSimulation}>Run Simulation</button>
-        <div className='histogram'>
-            <h1>Monte Carlo Simulation Histogram</h1>
-            {histogramData ? (
-            <Histogram
-                binEdges={histogramData.bin_edges}
-                frequencies={histogramData.frequencies}
-            />
-            ) : (
-            <p>Loading...</p>
-            )}
-        </div>
-        {/* <div className='resultsSummary'>
+      </div>
+      <div className="histogram">
+        {histogramData ? (
+          <Histogram
+            binEdges={histogramData.bin_edges}
+            frequencies={histogramData.frequencies}
+          />
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+      {/* <div className='resultsSummary'>
             {results && (
             <div>
                 <h3>Simulation Results</h3>
@@ -41,8 +48,8 @@ const GraphArea = ({ variables }) => {
             </div>
             )}
         </div> */}
-      </div>
-    );
-  };
-  
-  export default GraphArea;
+    </div>
+  );
+};
+
+export default GraphArea;
---
+++ frontend/src/components/Slider.js
@@ -0,0 +1,9 @@
+const Slider = ({ max, min }) => {
+  return (
+    <div>
+      <input type="range" min="1" max="100" />
+    </div>
+  );
+};
+
+export default Slider;
